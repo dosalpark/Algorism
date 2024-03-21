f3a0b5d4e890598a79fffcdb8931e6bdc1395e47
dosalpark <154612223+dosalpark@users.noreply.github.com>
2024-03-21 10:31:49 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,9 +1,6 @@
 -- 코드를 입력하세요
-SELECT I.ANIMAL_ID
-     , I.NAME
-FROM ANIMAL_INS AS I
-    INNER JOIN ANIMAL_OUTS AS O ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE I.DATETIME > O.DATETIME
-ORDER BY I.DATETIME;
-    
-    
+SELECT i.animal_id, i.name
+FROM animal_ins i JOIN animal_outs o ON i.animal_id = o.animal_id
+WHERE i.datetime > o.datetime
+ORDER BY i.datetime ;
+
