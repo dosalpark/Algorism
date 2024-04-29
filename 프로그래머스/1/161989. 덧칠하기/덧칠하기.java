2f1e6bd26da02a5ee9d9dad45e4383c2cc2c6508
dosalpark <154612223+dosalpark@users.noreply.github.com>
2024-04-29 16:44:02 +0900

    [level 1] Title: 덧칠하기, Time: 1.89 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int painting = 0; //5
+        int painting = 0;
         Arrays.sort(section);
-
 
         for (int i = 0; i < section.length; i++) {
             if(section[i] > painting){
