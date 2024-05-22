bd41a5fbc482b3b029a22cccefd391467c5934c9
박승현 <psh_a@naver.com>
2024-05-22 17:00:25 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,33 +1,56 @@
-
+import java.util.*;
 class Solution {
-    public int solution(int storey) {
+    public int bfs(int storey){
         int answer = 0;
+
+        Queue<Elevator> que = new LinkedList<>();
+        que.add(new Elevator(storey,0));
+
         int floor = 1;
-        while (true){
-            if(storey == 0){
-                break;
-            }
+        while(!que.isEmpty()){
+            Elevator elevator = que.poll();
+            int now = elevator.getNow();
+            int count = elevator.getCount();
+            System.out.println("now: " + now + "/ count: " + count);
 
-            if(storey%10 == 0){
+            if(now == 0){
+                return count;
+            }
+            if(now%10 == 0){
                 while(true){
-                    storey /= 10;
-                    if(storey%10 != 0) break;
+                    now /= 10;
+                    if(now%10 != 0) break;
+                    System.out.println("now: " + now + "/ count: " + count);
                 }
             }
-
-            if(storey%10 > 5){
-                storey += floor;
-            } else if(storey%10 < 5){
-                storey -= floor;
-            } else {
-                if(storey%100 >= 50){
-                    storey += floor;
-                } else {
-                    storey -= floor;
-                }
+            if(now%10 > 5){
+                que.add(new Elevator(now + floor, count + 1));
+            }else{
+                que.add(new Elevator(now - floor, count + 1));
             }
-            answer++;
         }
         return answer;
+    }
+
+
+    public int solution(int storey) {
+        return bfs(storey);
+    }
+}
+
+class Elevator {
+    public int now;
+    public int count;
+
+    public Elevator(int now, int count){
+        this.now = now;
+        this.count = count;
+    }
+
+    public int getNow(){
+        return this.now;
+    }
+    public int getCount(){
+        return this.count;
     }
 }
