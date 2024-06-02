aa379d403ab5e3c9cd70bb42a2ebc23733cec808
박승현 <psh_a@naver.com>
2024-06-02 17:12:08 +0900

    [level 2] Title: 땅따먹기, Time: 17.63 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,23 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        int[][] dp = new int[land.length+1][4];
+
+        for(int i = 1; i <= land.length; i++){
+            for(int j = 0; j < 4; j++){
+                for(int k = 0; k < 4; k++){
+                    if(j == k) continue;
+                    
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + land[i-1][j]);
+                    answer = Math.max(answer,dp[i][j]);
+                        
+                }
+               
+            }
+            
+        }
+
+        return answer;
+    }
+}
