aa1462cab708517a4c3ea8b70eab2e0cd92a93b1
박승현 <psh_a@naver.com>
2024-07-11 16:48:21 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,18 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-    int[] answer = new int[photo.length];
-        Map<String ,Integer> nameScore = new HashMap<>();
-        for (int i = 0; i < name.length; i++) {
-            nameScore.put(name[i],yearning[i]);
+        int[] answer = new int[photo.length];
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++){
+            map.put(name[i], yearning[i]);
         }
-
-        for(int i = 0; i < photo.length; i++)
-            for (int j = 0; j < photo[i].length; j++){
-                if(nameScore.containsKey(photo[i][j])){
-                    answer[i] += nameScore.get(photo[i][j]);
-                }
+        
+        for(int i = 0; i < photo.length; i++){
+            int sum = 0;
+            for(int j = 0; j < photo[i].length; j++){
+                sum += map.getOrDefault(photo[i][j],0);
             }
+            answer[i] = sum;
+        }
+        
         return answer;
     }
 }
