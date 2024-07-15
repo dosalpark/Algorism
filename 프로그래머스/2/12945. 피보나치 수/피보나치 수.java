78e2e2364b0adb436681d62e1cfe947a0b11a38e
박승현 <psh_a@naver.com>
2024-07-15 21:01:54 +0900

    [level 2] Title: 피보나치 수, Time: 2.46 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int[] arr = new int[n+1]; //012
         
-        int[] nArr = new int[n +1];
-        nArr[0] = 0;
-        nArr[1] = 1;
-        nArr[2] = 1;
-
-        for (int i = 3; i < nArr.length; i++){
-            nArr[i] = (nArr[i-1] + nArr[i-2]) % 1234567;
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
         }
-        answer = nArr[n];
-        return answer;
+        return arr[n];
     }
 }
