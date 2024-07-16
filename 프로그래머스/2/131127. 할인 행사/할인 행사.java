4ab3a32287614fc6570e00bdee0969456cd5517b
박승현 <psh_a@naver.com>
2024-07-16 15:28:26 +0900

    [level 2] Title: 할인 행사, Time: 82.11 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,26 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        List<String> wishList = new ArrayList<>();
-        for(int i = 0; i < number.length; i++){
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < want.length; i++){
             for(int j = 0; j < number[i]; j++){
-                wishList.add(want[i]);
+                list.add(want[i]);
             }
         }
+        
+        for(int i = 0; i < discount.length -9; i++){
 
-
-        for (int i = 0; i <= discount.length -10; i++){
-
-            List<String> day10Discount = 
-                new ArrayList<>(List.of(Arrays.copyOfRange(discount,i,i+10)));
-            
-            for (String s : wishList) {
-                day10Discount.remove(s);
+            List<String> copyList = new ArrayList<>(list);
+            for(int j = i; j < i+10; j++){
+                if(copyList.contains(discount[j])){
+                    copyList.remove(discount[j]);
+                } else {
+                    break;
+                }
             }
-            if(day10Discount.isEmpty()){
-                answer++;
-            }
+            if(copyList.isEmpty()) answer++;
         }
+        
         return answer;
     }
 }
+
+//13-9
+//4
+
