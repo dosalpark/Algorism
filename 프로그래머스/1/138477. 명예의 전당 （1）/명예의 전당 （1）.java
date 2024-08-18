b741412c9807fd15f5fbfa829883be9f0f4bca1b
박승현 <psh_a@naver.com>
2024-08-19 00:38:20 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.17 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,18 +3,13 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int min = 2001;
         
         for(int i = 0; i < score.length; i++){
-            if(i < k){
-                pq.add(score[i]);
-            } else {
-                pq.add(score[i]);
+            pq.add(score[i]);
+            if(i >= k){
                 pq.poll();
             }
-            int cur = pq.poll();
-            answer[i] = cur;
-            pq.add(cur);
+            answer[i] = pq.peek();
         }
         
         return answer;
