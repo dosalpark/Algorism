25f6112ed8902819e1d8fb10c0db40b15e37b02d
박승현 <psh_a@naver.com>
2024-08-24 01:55:04 +0900

    [level 2] Title: 피로도, Time: 4.97 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,36 @@
 class Solution {
-    int answer;
 
-    public void dfs(int k, int[][] dungeons, boolean[] visited, int sum){
-        answer = Math.max(answer, sum);
-
-        for(int i = 0; i < dungeons.length; i++){
-            if(!visited[i] && k >= dungeons[i][0]){
-                visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, visited, sum +1);
-                visited[i] = false;
-            }
-        }
-    }
+    int[][] dungeons;
 
     public int solution(int k, int[][] dungeons) {
-        answer = 0;
-        boolean[] visited = new boolean[dungeons.length];
-        dfs(k, dungeons, visited, 0);
+        int answer = 0;
+        this.dungeons = dungeons;
 
+        for (int i = 0; i < dungeons.length; i++) {
+            boolean[] visit = new boolean[dungeons.length];
+            int sum = dfs(k, visit, i, 0);
+            answer = Math.max(sum, answer);
+        }
         return answer;
+    }
+
+    private int dfs(int k, boolean[] visit, int in, int sum) {
+        if (dungeons[in][0] <= k) {
+            visit[in] = true;
+            k -= dungeons[in][1];
+            sum += 1;
+        } else {
+            return sum;
+        }
+
+        int max = sum;
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visit[i]) {
+                max = Math.max(max,dfs(k, visit, i, sum));
+            }
+        }
+        visit[in] = false;
+        return max;
     }
 }
