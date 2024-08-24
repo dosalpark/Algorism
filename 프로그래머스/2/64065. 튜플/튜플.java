8e78c264bdce794388d04b6b7ec2ea7bb3a829b6
박승현 <psh_a@naver.com>
2024-08-24 14:38:17 +0900

    [level 2] Title: 튜플, Time: 57.49 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,30 +1,30 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
+
     public int[] solution(String s) {
-        Map<String, Integer> resultMap = new HashMap<>();
-        
-        s = s.substring(2,s.length() -2);
-        String[] sArray = s.split("\\},\\{");
-        
-        for(String str : sArray){
-            String[] numArr = str.split(",");
-            for(String num : numArr){ 
-                resultMap.put(num, resultMap.getOrDefault(num, 0) + 1);    
+        int[] answer = {};
+        Set<String> set = new LinkedHashSet<>();
+        String[] arr = s.substring(2, s.length() - 2).split("\\},\\{");
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2){
+                return o1.length() - o2.length();
+            }
+        });
+
+        for (String string : arr) {
+            for(String str : string.split(",")){
+                set.add(str);
             }
         }
-        
-        List<String> keyList = new ArrayList<>(resultMap.keySet());
-        keyList.sort((s1, s2) -> resultMap.get(s2) - resultMap.get(s1));
-        
-        int[] answer = new int[keyList.size()];
-            
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = Integer.parseInt(keyList.get(i));
+
+        answer = new int[set.size()];
+        int i = 0;
+        for (String string : set) {
+            answer[i] = Integer.parseInt(string);
+            i++;
         }
-        
+
         return answer;
     }
 }
