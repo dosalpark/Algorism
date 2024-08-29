b2bd471374c56babb06284e3515347ec8e0f23ea
박승현 <psh_a@naver.com>
2024-08-29 20:53:49 +0900

    [level 1] Title: K번째수, Time: 0.61 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,14 +2,24 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for (int j = 0; j < commands.length; j++){
-            List<Integer> list = new ArrayList<>();
-            for(int i = commands[j][0] -1; i < commands[j][1]; i++){
-                list.add(array[i]);
+        for(int i = 0; i < commands.length; i++){
+            pq.clear();
+            int start = commands[i][0] -1;
+            int end = commands[i][1] -1;
+            int target = commands[i][2];
+            
+            for(int j = start; j <= end; j++){
+                pq.add(array[j]);
             }
-            Collections.sort(list);
-            answer[j] = list.get(commands[j][2]-1);
+            int t = 0;
+            for(int j = 0; j < target; j++){
+                t = pq.poll();
+            }
+            
+            answer[i] = t;
+            
         }
         
         return answer;
