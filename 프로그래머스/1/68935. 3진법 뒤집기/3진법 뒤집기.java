b8acf445608566c94370355e8343e4863e37addf
박승현 <psh_a@naver.com>
2024-09-01 12:06:37 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,23 +1,9 @@
-import java.util.Stack;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        Stack<Integer> stack = new Stack<Integer>();
-        
-        while(true){
-            stack.add(n%3);
-            n = n/3;
-            
-            if(n == 0){
-                break;
-            }
-        }
-        
-        int size = stack.size();
-        
-        for (int i = 0; i < size; i++){
-            answer += stack.pop() * (Math.pow(3,i));
-        }
+
+        String three = Integer.toString(n, 3);
+        String threeReverse = new StringBuilder(three).reverse().toString();
+        int answer = Integer.parseInt(threeReverse,3);
 
         return answer;
     }
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,14 +1,14 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.15 ms
+메모리: 79.4 MB, 시간: 0.12 ms
 
 ### 구분
 
-코딩테스트 연습 > 월간 코드 챌린지 시즌1
+ > 
 
 ### 채점결과
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 12:24:48
+2024년 09월 01일 12:06:35
 
 ### 문제 설명
 
