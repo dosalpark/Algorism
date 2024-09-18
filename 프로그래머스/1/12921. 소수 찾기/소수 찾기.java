3ccd620f1c843043a0c2a9cfa74924bd262b29c9
박승현 <psh_a@naver.com>
2024-09-18 18:37:51 +0900

    [level 1] Title: 소수 찾기, Time: 216.42 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,21 +1,18 @@
 class Solution {
-    
     public int solution(int n) {
         int answer = 0;
         
         for(int i = 2; i <= n; i++){
             boolean flag = true;
-            for(int j = 2; j <= (int)Math.sqrt(i); j++){
-                if(i%j == 0){
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i % j == 0){
                     flag = false;
                     break;
-                }
+                }    
             }
-            if(flag){
-                answer++;    
-            }
+            if(flag) answer++;
         }
-        
+            
         return answer;
     }
 }
