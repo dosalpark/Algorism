238bfe872a57681023454460dd13b841e5d7aa5a
박승현 <psh_a@naver.com>
2024-09-18 20:22:12 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        while(n>=a){
-            int gift = n/a * b;
-            n = n%a;
-    
-            answer += gift;
-            n += gift;
-           
+        while(true){
+            if(n < a){
+                break;
+            }
+            int check = (n / a) * b;
+            answer += check;
+            n = (n % a) + check;
         }
         return answer;
     }
