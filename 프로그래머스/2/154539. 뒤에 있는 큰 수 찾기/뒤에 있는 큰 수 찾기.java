ddcc2ea67faa7e90070f56e1624809446c38b01f
박승현 <psh_a@naver.com>
2024-09-28 18:38:46 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 122.80 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,20 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = numbers.length-1; i >= 0; i--){
-            while (!stack.isEmpty() && numbers[i] >= stack.peek()){
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            while (!stack.empty() && stack.peek() <= numbers[i]) {
                 stack.pop();
             }
-            if(stack.isEmpty()){
-                answer[i] = -1;
-            } else {
+
+            if (!stack.empty()) {
                 answer[i] = stack.peek();
             }
-            stack.push(numbers[i]);
+            stack.add(numbers[i]);
         }
-            return answer;
+        
+        return answer;
     }
 }
