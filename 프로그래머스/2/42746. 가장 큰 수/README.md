6b0b0c6d6a77575c0e8418203b23722222bf4def
박승현 <psh_a@naver.com>
2024-10-02 14:20:37 +0900

    [level 2] Title: 가장 큰 수, Time: 156.49 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,14 +1,14 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#qna) 
 
 ### 성능 요약
 
-메모리: 81 MB, 시간: 2.12 ms
+메모리: 124 MB, 시간: 156.49 ms
 
 ### 구분
 
-코딩테스트 연습 > 정렬
+ > 
 
 ### 채점결과
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 19일 17:36:15
+2024년 10월 02일 14:20:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,33 +1,47 @@
-import java.util.*;
-class Solution {    
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+
     public String solution(int[] numbers) {
-        String[] strNumbers = new String[numbers.length];
+        Integer[] num = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        String[] numStr = Arrays.stream(num).map(String::valueOf).toArray(String[]::new);
+        Arrays.sort(numStr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                while (true) {
+                    if (o1.length() == o2.length()) {
+                        return o2.compareTo(o1);
+                    } else if (o1.length() < o2.length()) {
+                        for (int i = 0; i < o1.length(); i++) {
+                            if (o1.charAt(i) != o2.charAt(i)) {
+                                return o2.substring(i, i + 1).compareTo(o1.substring(i, i + 1));
+                            }
+                        }
+                        o2 = o2.substring(o1.length());
+                    } else {
+                        for (int i = 0; i < o2.length(); i++) {
+                            if (o1.charAt(i) != o2.charAt(i)) {
+                                return o2.substring(i, i + 1).compareTo(o1.substring(i, i + 1));
+                            }
+                        }
+                        o1 = o1.substring(o2.length());
+                    }
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : numStr) {
+            sb.append(str);
+        }
         int zeroCount = 0;
-        for (int i = 0; i < numbers.length; i++){
-            strNumbers[i] = String.valueOf(numbers[i]);
-            if(numbers[i] == 0){
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '0') {
                 zeroCount++;
             }
         }
-        if(zeroCount == numbers.length){
-            return "0";
-        }
-        
-        Arrays.sort(strNumbers, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2){
-                String a = o1 + o2;
-                String b = o2 + o1;
-                return b.compareTo(a);
-            }
-        });
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(String strNumber : strNumbers){
-            sb.append(strNumber);
-        }
-        
-        return sb.toString();
+
+        return sb.length() != zeroCount ? sb.toString() : "0";
     }
 }
