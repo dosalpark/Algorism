647cb6db4795bfe48fcbb4d185a37b5f3bbfc396
박승현 <psh_a@naver.com>
2024-10-10 22:25:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,15 @@
 class Solution {
     public long solution(int n) {
-        if(n == 1){
-            return 1;
-        } else if(n == 2) {
-            return 2;
+        long answer = 1;
+        long n1 = 1; 
+        long n2 = 1; 
+        
+        for (int i = 1; i < n; i++) {
+            answer = (n1 + n2) % 1234567;
+            n1 = n2;
+            n2 = answer;
         }
-        long[] nArr = new long[n+1];
-        nArr[0] = 0;
-        nArr[1] = 1;
-        nArr[2] = 2;
-
-        for(int i = 3; i <= n; i++){
-            nArr[i] = (nArr[i-1] + nArr[i-2]) % 1234567;
-        }
-
-        return nArr[n];
+        
+        return answer;
     }
 }
-
-
