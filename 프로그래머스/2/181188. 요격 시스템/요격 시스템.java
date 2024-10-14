f5636e0c960d254e521f459cd128e4d2d4837602
박승현 <psh_a@naver.com>
2024-10-14 17:57:14 +0900

    [level 2] Title: 요격 시스템, Time: 449.26 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
 
-import java.util.*;
 class Solution {
+
     public int solution(int[][] targets) {
-        int answer = 0;
-        int a = 0;
-        
-    
+        int answer = 1;
+
         Arrays.sort(targets, (o1, o2) -> {
-            return o1[1] - o2[1];
-        });
-        
-        for(int[] target : targets){            
-            if(a <= target[0]){
-                a = target[1];
-                answer++;
-                // System.out.println("answer: " + answer);  
-            }        
-            // System.out.println("[0]- " + target[0] + " / [1]- " + target[1]);
+                int one = o1[1];
+                int two = o2[1];
+                if (one == two) {
+                    return o1[0] - o2[0];
+                } else {
+                    return one - two;
+                }
+            }
+        );
+        int min = targets[0][1];
+        for(int i = 1; i < targets.length; i++){
+            if(targets[i][0] < min){
+                continue;
+            }
+            answer++;
+            min = targets[i][1];
         }
-        
         return answer;
     }
 }
