bd4b90a5bdf5a00f4d629e2a9e9195b89b9af4af
박승현 <psh_a@naver.com>
2024-10-19 09:43:40 +0900

    [level 2] Title: [3차] n진수 게임, Time: 97.45 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,23 +1,19 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        String answer = "";
-        int max = t * m;
-        StringBuilder sb = new StringBuilder();
-        //총 구해야하는 갯수 t*m
-        for(int i = 0; i < 99999; i++){
-            String converter = Integer.toString(i,n).toUpperCase();
-            sb.append(converter);
-            
-            if(max < sb.length()) break;
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < t*m; i++){
+            result.append(Integer.toString(i,n));
         }
-        
-        for(int i = p-1; i < sb.length(); i = i + m){
-            if(t == 0) break;
-            answer+= "" + sb.charAt(i);
-            t--;
+        String[] arr = result.toString().split("");
+        result.setLength(0);
+
+        for(int i = p-1; i < arr.length; i=i+m){
+            result.append(arr[i]);
+            if(result.length()==t){
+                break;
+            }
         }
-        
-        
-        return answer;
+
+        return result.toString().toUpperCase();
     }
 }
