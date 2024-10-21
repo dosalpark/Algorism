ddc3b62144a4fb7ca035837d0c5325e303ca4ee2
박승현 <psh_a@naver.com>
2024-10-21 15:02:35 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 65.30 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,40 +1,62 @@
-import java.util.*;
-class Solution {    
-    public boolean divisionFail(int[] array, int i){
-        for(int now : array){
-            if(now%i == 0){
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        int gcbA = arrayA[0];
+        int gcbB = arrayB[0];
+        List<Integer> gcbAllA = gcbAll(gcbA);
+        List<Integer> gcbAllB = gcbAll(gcbB);
+
+        for (int i = 0; i < Math.max(gcbAllA.size(), gcbAllB.size()); i++) {
+            if (i < gcbAllA.size()) {
+                if (canDivision(arrayA, gcbAllA.get(i)) && cantDivision(arrayB, gcbAllA.get(i))) {
+                    answer = Math.max(gcbAllA.get(i), answer);
+                }
+
+            }
+            if (i < gcbAllB.size()) {
+                if (canDivision(arrayB, gcbAllB.get(i)) && cantDivision(arrayA, gcbAllB.get(i))) {
+                    answer = Math.max(gcbAllB.get(i), answer);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private List<Integer> gcbAll(int gcb) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(gcb); i++) {
+            if (gcb % i == 0) {
+                list.add(i);
+                list.add(gcb / i);
+            }
+        }
+        return list;
+    }
+
+    private boolean cantDivision(int[] array, int gcb) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % gcb == 0) {
                 return false;
             }
         }
         return true;
     }
-    
-    public int gcd(int arrayNum, int num){
-        if(num==0) return arrayNum;
-        return gcd(num, arrayNum%num);
-    }
-    
-    public int solution(int[] arrayA, int[] arrayB) {
-        int answer = 0;
-        Arrays.sort(arrayA);
-        Arrays.sort(arrayB);
-        
-        int gcdA = arrayA[0];
-        int gcdB = arrayB[0];
-        
-        
-        for(int i = 1; i < arrayA.length; i++){
-            gcdA = gcd(arrayA[i], gcdA);
-            gcdB = gcd(arrayB[i], gcdB);
-        }       
-        
-        if(divisionFail(arrayB, gcdA)){
-            answer = Math.max(answer,gcdA);
+
+    private boolean canDivision(int[] array, int gcb) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % gcb != 0) {
+                return false;
+            }
         }
-        if(divisionFail(arrayA, gcdB)){
-            answer = Math.max(answer,gcdB);
-        }
-        
-        return answer;
+        return true;
     }
+
 }
