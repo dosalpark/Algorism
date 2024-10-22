a6b50b511f39330c0aa0f0f86e5528aa94546f24
박승현 <psh_a@naver.com>
2024-10-22 15:26:17 +0900

    [level 2] Title: 스킬트리, Time: 0.06 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,26 +2,30 @@ class Solution {
 
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-
-        for (String tree : skill_trees) {
-            boolean flag = true;
-            int index = 0;
-            for (int i = 0; i < tree.length(); i++) {
-                if(skill.indexOf(tree.charAt(i)) == -1){
-                    continue;
-                }
-
-                if(skill.charAt(index) == tree.charAt(i)){
-                    index++;
-                } else {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
+        for (String skill_tree : skill_trees) {
+            if (check(skill, skill_tree)) {
                 answer++;
             }
         }
         return answer;
+    }
+
+    private boolean check(String skill, String skillTree) {
+
+        while (!skill.isEmpty() && !skillTree.isEmpty()) {
+            char ch = skillTree.charAt(0);
+            int turn = skill.indexOf(ch);
+            if (turn == -1) {
+                skillTree = skillTree.substring(1);
+            } else if (turn > 0) {
+                return false;
+            } else {
+                skillTree = skillTree.substring(1);
+                skill = skill.substring(1);
+            }
+        }
+
+        return true;
+
     }
 }
