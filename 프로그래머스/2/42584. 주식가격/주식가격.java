135377d47e8ff948a5ec2d1ca97e905d12a17634
박승현 <psh_a@naver.com>
2024-11-04 11:25:22 +0900

    [level 2] Title: 주식가격, Time: 20.17 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,16 @@
 class Solution {
+
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
 
-        for(int i = 0 ; i < prices.length; i++){
-            int now = prices[i];
+        for (int i = 0; i < prices.length; i++) {
             int count = 0;
-            for(int j = i+1; j < prices.length; j++){
+            for (int j = i + 1; j < prices.length; j++) {
                 count++;
-                if(now > prices[j]) break; 
-
+                if (prices[i] > prices[j]) {
+                    break;
+                }
             }
-            
             answer[i] = count;
         }
         return answer;
