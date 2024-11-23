a5a719dcfbf9b88202e6f24dd4b4254694c0a108
박승현 <psh_a@naver.com>
2024-11-23 19:24:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println("0");
+        } else {
+            System.out.println("" + fibonacci(N));
+        }
+
+
+    }
+
+    private static int fibonacci(int n) {
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
