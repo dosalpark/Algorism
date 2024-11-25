ce6ef4e2589a1e74f80aa51c8ee38b0a177d37f9
박승현 <psh_a@naver.com>
2024-11-25 23:50:24 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23468 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int re = 1, dp = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+        dp[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
