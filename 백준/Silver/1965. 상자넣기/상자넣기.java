d9ab7c1c312a9862f84408cf0f74486841f2f9e8
박승현 <psh_a@naver.com>
2024-12-23 18:57:33 +0900

    [Silver II] Title: 상자넣기, Time: 132 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] box = new int[N];
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < N; i++) {
+            box[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (box[i] > box[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i : dp) {
+            max = Math.max(max,i);
+        }
+
+        bw.write(max + "");
+        bw.flush();
+        bw.close();
+    }
+}
